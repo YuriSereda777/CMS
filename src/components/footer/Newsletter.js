8c3962a41f95a2f0bb821649b5d7705c4881034b
YuriSereda777 <lomaj3ch@hotmail.com>
2023-03-22 06:03:17 +0200

    .

--- src/components/footer/Newsletter.js
+++ src/components/footer/Newsletter.js
@@ -8,8 +8,8 @@ const Newsletter = () => {
         <h5 className="font-weight-bold mb-2">Our Newsletter</h5>
         <p className='mb-3'>Subscribe to receive all new updates about our website!</p>
         <form>
-          <div class="input-group">
-            <input type="email" class="form-control" placeholder="Email" style={{borderRadius: '5px 0 0 5px'}} />
+          <div className="input-group">
+            <input type="email" className="form-control" placeholder="Email" style={{borderRadius: '5px 0 0 5px'}} />
             <Button type="submit" className="btn-primary" text='Subscribe' style={{fontSize: '14px', borderRadius: '0 5px 5px 0'}} />
           </div>
         </form>
