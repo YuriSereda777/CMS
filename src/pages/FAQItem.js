f22e7ef17a082fb1eb0fb96c579302a3c6358992
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-09 03:49:19 +0200

    Responsive Navbar & Better FAQ

--- src/pages/FAQItem.js
+++ src/pages/FAQItem.js
@@ -1,55 +1,60 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom'
-import Alert from '../UI/Alert';
-import Hero from '../UI/Hero'
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import Alert from "../UI/Alert";
+import Hero from "../UI/Hero";
+import faq from "./FAQ-Questions";
 
-import classes from './FAQItem.module.css';
+import classes from "./FAQItem.module.css";
+import Questions from "./Questions";
 
 const FAQItem = () => {
+  const [questions, setQuestions] = useState(faq);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  };
+
   const params = useParams();
   return (
     <>
-      <Hero title='Frequently Asked Questions' />
-      <section className={`${classes['faq-item']} faq-item py-5`}>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-auto p-0'>
-              <Link to='/faq'>
-                <i className={`fa-solid fa-arrow-left-long ${classes.arrow}`}></i>
+      <Hero title="Frequently Asked Questions" />
+      <section className={`${classes["faq-item"]} faq-item py-5`}>
+        <div className="container w-90vw">
+          <div className="row">
+            <div className="col-auto p-0">
+              <Link to="/faq">
+                <i
+                  className={`fa-solid fa-arrow-left-long ${classes.arrow}`}
+                ></i>
               </Link>
             </div>
-            <div className='col mb-4'>
+            <div className="col mb-4">
               <h1>{params.item}</h1>
             </div>
           </div>
-          <div className='row'>
-            <div className='col-8 pe-5'>
-              <div className={classes.qa}>
-              <p>Q. Pariatur excepteur ipsum laboris tempor?</p>
-              <p>A. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend sapien a bibendum pellentesque. Ut vitae pharetra eros, vitae suscipit nulla.</p>
-              </div>
-              <div className={classes.qa}>
-                <p>Q. Incididunt nisi commodo nostrud commodo adipisicing excepteur laboris amet?</p>
-                <p>A. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend sapien a bibendum pellentesque. Ut vitae pharetra eros, vitae suscipit nulla. Vestibulum placerat ex non justo finibus eleifend. Sed ullamcorper sit amet enim et placerat. Quisque vulputate erat feugiat erat ultricies gravida.</p>
-              </div>
-              <div className={classes.qa}>
-                <p>Q. Magna duis sunt sunt qui consectetur?</p>
-                <p>A. In congue justo nisl, eu viverra lacus auctor sed. Duis sem lorem, venenatis vitae commodo a, porta vehicula est. Ut eget turpis scelerisque, hendrerit nisl sed, vehicula dui. Duis orci odio, congue vel dictum vel, tempus quis turpis.</p>
-              </div>
-              <div className={classes.qa}>
-                <p>Q. Fugiat ex eiusmod laboris pariatur ea?</p>
-                <p>A. Cillum veniam esse nisi elit elit occaecat irure do ullamco pariatur excepteur.</p>
-              </div>
+          <div className="row">
+            <div className="col-md-8 pe-5">
+              <Questions
+                questions={questions}
+                activeId={activeId}
+                toggleQuestion={toggleQuestion}
+              />
             </div>
-            <div className='col-4'>
-              <Alert path='/create-complaint' icon>Couldn't find your answer? Create a new complaint from here.</Alert>
-              <Alert path='/my-complaints' icon>View your previously created complaints from here.</Alert>
+            <div className="col-4 ">
+              <Alert path="/create-complaint" icon>
+                Couldn't find your answer? Create a new complaint from here.
+              </Alert>
+              <Alert path="/my-complaints" icon>
+                View your previously created complaints from here.
+              </Alert>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default FAQItem
+export default FAQItem;
---
+++ src/pages/SingleQuestion.js
@@ -0,0 +1,24 @@
+import "./Questions.css";
+
+const SingleQuestion = ({ id, title, answer, activeId, toggleQuestion }) => {
+  const isActive = id === activeId;
+  return (
+    <article
+      className="question shadow my-3"
+      onClick={() => toggleQuestion(id)}
+    >
+      <header className="d-flex justify-content-between align-items-center">
+        <h5>{title}</h5>
+        <button className="question-btn">
+          {isActive ? (
+            <i className="fa-solid fa-xmark fa-xl"></i>
+          ) : (
+            <i className="fa-light fa-plus fa-2xl"></i>
+          )}
+        </button>
+      </header>
+      {isActive && <p>{answer}</p>}
+    </article>
+  );
+};
+export default SingleQuestion;
