03abadbc91327f2d8071f33ed56f333de016b207
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-09 13:41:18 +0200

    Made the pagination dynamic

--- src/UI/Pagination.js
+++ src/UI/Pagination.js
@@ -1,20 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 import classes from './Pagination.module.css'
 
-const Pagination = () => {
+const Pagination = ({ pagesNumber, currentPage }) => {
+  const location = useLocation();
+
+  let path = location.pathname.split('/');
+  console.log(location.pathname)
+  path.pop();
+  path = path.join('/') + '/';  
+
+  const pages = Array.from({length: pagesNumber}, (_, index) => {
+    return index + 1
+  });
+
+  currentPage = parseInt(currentPage);
+  console.log(currentPage)
+  pagesNumber = parseInt(pagesNumber);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isNaN(currentPage) || currentPage < 1) {
+      navigate(path + '1');
+    } else if (currentPage > pagesNumber) {
+      navigate(path + pagesNumber);
+    } else if (!currentPage) {
+      if (location.pathname.charAt(location.pathname.length - 1) === '/') {
+        navigate(location.pathname + '1')
+      } else {
+        navigate(location.pathname + '/1');
+      }
+    }
+  }, [pagesNumber, currentPage])
+
+  if ( pagesNumber < 2 ) {
+    return;
+  }
+
   return (
     <ul className={'d-flex justify-content-center mt-4'}>
-      <li>
-        <Link className={`${classes['page-link']}`} to="#">«</Link>
-      </li>
-      <li><Link className={`${classes['page-link']}`} to="1">1</Link></li>
-      <li><Link className={`${classes['page-link']} ${classes.active} `} to="2">2</Link></li>
-      <li><Link className={`${classes['page-link']}`} to="3">3</Link></li>
-      <li>
-        <Link className={`${classes['page-link']}`} to="#">»</Link>
-      </li>
+      {
+        currentPage != 1 &&
+        <li>
+          <Link className={`${classes['page-link']}`} to={`${path}${currentPage - 1}`}>«</Link>
+        </li>
+      }
+
+      {
+        pages.map((page, index) => (
+          <li key={index}>
+            <Link 
+              className={currentPage === page ? classes['page-link'] + ' ' + classes.active : classes['page-link']} 
+              to={`${path}${page}`}>
+                {page}
+            </Link>
+          </li>
+        ))
+      }
+
+      {
+        currentPage != pagesNumber &&
+        <li>
+          <Link className={`${classes['page-link']}`} to={`${path}${currentPage + 1}`}>»</Link>
+        </li>
+      }
   </ul>
   )
 }
