aa59184044c79c4e29f168efa463d825ecf104f9
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-10 07:21:23 +0200

    Update Complaint.js

--- src/pages/admin/Complaint.js
+++ src/pages/admin/Complaint.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Complaint = () => {
+  const { id } = useParams();
+
   return (
-    <div>Complaint</div>
+    <div>{id}</div>
   )
 }
 
