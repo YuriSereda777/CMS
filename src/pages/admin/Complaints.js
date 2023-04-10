e2fb4db7e0b5ceeba66e51efab30264ccf933ff4
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-10 07:19:56 +0200

    Added complaints fetch function

--- src/pages/admin/Complaints.js
+++ src/pages/admin/Complaints.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Pagination from '../../UI/Pagination'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 const Complaints = () => {
-  const complaints = [
+  const [complaints, setComplaints] = useState([]);
+
+  const getComplaints = useCallback(async () => {
+    const response = await fetch('http://localhost:80/cms-api/complaints.php');
+
+    const data = await response.json();
+  
+    setComplaints(data);
+  }, []);
+
+  useEffect(() => {
+    getComplaints();
+  }, [getComplaints]);
+
+  const DUMMY_COMPLAINTS = [
     {
       id: 1,
       title: 'Lorem ipsum dolor sit amet',
@@ -209,10 +223,10 @@ const Complaints = () => {
   let { page: currentPage } = useParams();
 
   const elementsPerPage = 10;
-  const pagesNumber = Math.ceil(complaints.length / elementsPerPage) === 0 ? 1 :  Math.ceil(complaints.length / elementsPerPage);
+  const pagesNumber = Math.ceil(complaints.length / elementsPerPage) === 0 ? 1 : Math.ceil(complaints.length / elementsPerPage);
 
-  const start = (currentPage - 1) * elementsPerPage
-  const end = start + 10;
+  const start = (currentPage - 1) * elementsPerPage;
+  const end = start + elementsPerPage;
   
   return (
     <>
@@ -243,26 +257,28 @@ const Complaints = () => {
           {
             complaints.slice(start, end).map(complaint => 
               <div key={complaint.id} className='table-row py-3'>
-                <div className='row'>
-                  <div className='col-1'>
-                    <p>{complaint.id}</p>
+                <Link to={`/admin/complaint/${complaint.id}`}>
+                  <div className='row'>
+                    <div className='col-1'>
+                      <p>{complaint.id}</p>
+                    </div>
+                    <div className='col-3'>
+                      <p>{complaint.title}</p>
+                    </div>
+                    <div className='col-2'>
+                      <p>{complaint.categoryName}</p>
+                    </div>
+                    <div className='col-2'>
+                      <p>{complaint.userName}</p>
+                    </div>
+                    <div className='col-2'>
+                      <p>{complaint.date_created}</p>
+                    </div>
+                    <div className='col-2'>
+                      <p>{complaint.last_modified}</p>
+                    </div>
                   </div>
-                  <div className='col-3'>
-                    <p>{complaint.title}</p>
-                  </div>
-                  <div className='col-2'>
-                    <p>{complaint.category}</p>
-                  </div>
-                  <div className='col-2'>
-                    <p>{complaint.user}</p>
-                  </div>
-                  <div className='col-2'>
-                    <p>{complaint.createdAt}</p>
-                  </div>
-                  <div className='col-2'>
-                    <p>{complaint.lastModified}</p>
-                  </div>
-                </div>
+                </Link>
               </div>
             )
           }
