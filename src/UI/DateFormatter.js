687aaf957070896e4ed4a75d8e19e384953fb9c5
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-10 07:54:42 +0200

    Reusable Date Formatter

---
+++ src/UI/DateFormatter.js
@@ -0,0 +1,30 @@
+const DateFormatter = ({ dateFormatter }) => {
+  const date = new Date(dateFormatter);
+  const today = new Date();
+  const yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const isToday = date.toDateString() === today.toDateString();
+  const isYesterday = date.toDateString() === yesterday.toDateString();
+
+  let formattedDate = "";
+
+  formattedDate = isToday
+    ? `Today at ${date.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "2-digit",
+      })}`
+    : isYesterday
+    ? `Yesterday at ${date.toLocaleTimeString([], {
+        hour: "numeric",
+        minute: "2-digit",
+      })}`
+    : date.toLocaleString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+
+  return formattedDate;
+};
+export default DateFormatter;
