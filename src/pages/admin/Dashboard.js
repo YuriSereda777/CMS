10f30609881f44af7bb1937239e3f4a0c4b53e05
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-10 07:16:31 +0200

    Just a placeholder for the admin dashboard

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,8 +1,58 @@
 import React from 'react'
 
+import './Dashboard.css'
+
 const Dashboard = () => {
+  const cards = [
+    {
+      icon: 'fa-solid fa-users',
+      title: 'Users',
+      number: 50
+    },
+    {
+      icon: 'fa-solid fa-user-shield',
+      title: 'Admins',
+      number: 50
+    },
+    {
+      icon: 'fa-solid fa-user-shield',
+      title: 'Moderators',
+      number: 50
+    },
+    
+    {
+      icon: 'fa-solid fa-list',
+      title: 'Categories',
+      number: 50
+    },
+    {
+      icon: 'fa-solid fa-envelope',
+      title: 'Complaints',
+      number: 50
+    }
+  ]
+
   return (
-    <div>Dashboard</div>
+    <div className='admins'>
+      <h1 className='mb-5'>Dashboard</h1>
+      <div className='cards'>
+        <div className='row'>
+
+          {
+            cards.map((card, index) => (
+              <div className='col-2' key={index}>
+                <div className='card'>
+                  <i className={card.icon}></i>
+                  <p className='card-title'>{card.title}</p>
+                  <p className='card-number'>{card.number}</p>
+                </div>
+              </div>
+            ))
+          }
+
+        </div>
+      </div>
+    </div>
   )
 }
 
