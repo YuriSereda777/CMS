31a2aba49863c4e14370fcf9de2c7133097eb550
YuriSereda777 <lomaj3ch@hotmail.com>
2023-04-21 15:09:28 +0200

    Add files via upload

--- src/pages/admin/Complaints.js
+++ src/pages/admin/Complaints.js
@@ -1,53 +1,69 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from "react";
 import PaginationHandler from "../../UI/PaginationHandler";
-import { Link, useParams, useSearchParams } from 'react-router-dom'
-import DateFormatter from '../../UI/DateFormatter';
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import DateFormatter from "../../UI/DateFormatter";
 
-import dummyData from '../../dummy-data.json'
-import Input from '../../UI/Input';
-import Badge from '../../UI/Badge';
-import useSearch from '../../hooks/useSearch';
-import useHttp from '../../hooks/useHttp';
+import dummyData from "../../dummy-data.json";
+import Badge from "../../UI/Badge";
+import useSearch from "../../hooks/useSearch";
+import useHttp from "../../hooks/useHttp";
+import SearchBar from "../../UI/SearchBar";
+import SelectBar from "../../UI/SelectBar";
 
 const Complaints = () => {
   const [complaints, setComplaints] = useState([]);
-  const {isLoading, error, sendRequest: getComplaints} = useHttp();
-  const {setOriginalArray, filterArray, filteredArray, inputValue} = useSearch();
+  const { isLoading, error, sendRequest: getComplaints } = useHttp();
+  const { setOriginalArray, filterArray, filteredArray, inputValue } =
+    useSearch();
 
-  const dataHandler = useCallback((data) => {
-    setComplaints(data)
-    setOriginalArray(data);
-  }, [setOriginalArray]);
+  const dataHandler = useCallback(
+    (data) => {
+      setComplaints(data);
+      setOriginalArray(data);
+    },
+    [setOriginalArray]
+  );
 
   const searchHandler = (value) => {
-    filterArray('title', value)
+    filterArray("title", value);
   };
 
   useEffect(() => {
-    getComplaints({ url: 'http://localhost:80/cms-api/complaints.php' }, dataHandler);
+    getComplaints(
+      { url: "http://localhost:80/cms-api/complaints.php" },
+      dataHandler
+    );
   }, [getComplaints, dataHandler]);
 
-
-  const [sortBy, SetSortBy] = useState('id');
+  const [sortBy, SetSortBy] = useState("id");
 
   const sort = (arr) => {
-    if (sortBy === 'title') {
-      arr.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0))
-    } else if (sortBy === 'category') {
-      arr.sort((a,b) => (a.categoryName > b.categoryName) ? 1 : ((b.categoryName > a.categoryName) ? -1 : 0))
-    } else if (sortBy === 'user') {
-      arr.sort((a,b) => (a.userName > b.userName) ? 1 : ((b.userName > a.userName) ? -1 : 0))
-    } else if (sortBy === 'id') {
-      arr.sort((a,b) => a.id - b.id);
+    if (sortBy === "title") {
+      arr.sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0));
+    } else if (sortBy === "category") {
+      arr.sort((a, b) =>
+        a.categoryName > b.categoryName
+          ? 1
+          : b.categoryName > a.categoryName
+          ? -1
+          : 0
+      );
+    } else if (sortBy === "user") {
+      arr.sort((a, b) =>
+        a.userName > b.userName ? 1 : b.userName > a.userName ? -1 : 0
+      );
+    } else if (sortBy === "id") {
+      arr.sort((a, b) => a.id - b.id);
     } else {
-      arr.reverse()
+      arr.reverse();
     }
 
-    return arr
-  }
+    return arr;
+  };
 
   const sortHandler = (e) => {
-    SetSortBy(e.target.value)
+    SetSortBy(e.target.value);
+    console.log(e.target.value);
   };
 
   let { page: currentPage } = useParams();
@@ -63,126 +79,134 @@ const Complaints = () => {
           <p className="error-message">Found 0 records.</p>
         </div>
       </div>
-    )
+    );
   }
-  
+
   return (
     <>
-      <div className='row'>
-        <div className='col-12'>
-          <h1 className='mb-4'>Complaints</h1>
-          <div className='mb-3'>
+      <div className="row">
+        <div className="col-12">
+          <h1 className="mb-4">Complaints</h1>
+          <div className="mb-3">
             <form>
-              <div className='row'>
-                <div className='col-2 ps-0'>
-                  <Input
-                    className='search-input py-2'
-                    type='text'
-                    id='title'
-                    placeholder='Search'
+              <div className="row">
+                <div className="col-lg-4 col-sm-6 col-xs-12 ps-0 pd">
+                  <SearchBar
+                    className="search-input py-2"
+                    type="text"
+                    id="title"
+                    placeholder="Search"
                     value={inputValue}
                     onChange={(e) => searchHandler(e.target.value)}
                   />
                 </div>
-                <div className='col-2 ps-0'>
-                  <select className="form-control" onChange={sortHandler}>
-                    <option value='id'>Id</option>
-                    <option value='title'>Title</option>
-                    <option value='category'>Category</option>
-                    <option value='user'>User</option>
-                  </select>
+                <div className="col-lg-4 col-sm-6 col-xs-12 ps-0 pd">
+                  <SelectBar onChange={sortHandler} />
                 </div>
-                <div className='col-auto d-flex align-items-center'>
-                  <ul className=' d-flex'>
-                    <li><Badge className='me-2' text='Pending' /></li>
-                    <li><Badge text='Closed' /></li>
+                <div className="col-auto d-flex align-items-center">
+                  <ul className=" d-flex">
+                    <li>
+                      <Badge className="me-2" text="Pending" />
+                    </li>
+                    <li>
+                      <Badge text="Closed" />
+                    </li>
                   </ul>
                 </div>
               </div>
             </form>
           </div>
-          <div className='table-heading row d-none d-lg-flex py-3'>
-            <div className='col-1'>
+          <div className="table-heading row d-none d-lg-flex py-3">
+            <div className="col-1">
               <p>ID</p>
             </div>
-            <div className='col-3'>
+            <div className="col-3">
               <p>Title</p>
             </div>
-            <div className='col-2'>
+            <div className="col-2">
               <p>Category</p>
             </div>
-            <div className='col-2'>
+            <div className="col-2">
               <p>User</p>
             </div>
-            <div className='col-2'>
+            <div className="col-2">
               <p>Created At</p>
             </div>
-            <div className='col-2'>
+            <div className="col-2">
               <p>Last Modified</p>
             </div>
           </div>
 
-          {
-            sort(filteredArray, sortBy).slice(start, end).map(complaint => 
-              <div key={complaint.id} className='table-row py-3'>
+          {sort(filteredArray, sortBy)
+            .slice(start, end)
+            .map((complaint) => (
+              <div key={complaint.id} className="table-row py-3">
                 <Link to={`/admin/complaint/${complaint.id}`}>
-                  <div className='row'>
-                    <div className='d-none d-lg-block col-1'>
+                  <div className="row">
+                    <div className="d-none d-lg-block col-1">
                       <p>{complaint.id}</p>
                     </div>
-                    <div className='d-none d-lg-block col-3'>
+                    <div className="d-none d-lg-block col-3">
                       <p>{complaint.title}</p>
                     </div>
-                    <div className='d-none d-lg-block col-2'>
+                    <div className="d-none d-lg-block col-2">
                       <p>{complaint.categoryName}</p>
                     </div>
-                    <div className='d-none d-lg-block col-2'>
+                    <div className="d-none d-lg-block col-2">
                       <p>{complaint.userName}</p>
                     </div>
-                    <div className='d-none d-lg-block col-2'>
+                    <div className="d-none d-lg-block col-2">
                       <p>
                         <DateFormatter date={complaint.date_created} />
                       </p>
                     </div>
-                    <div className='d-none d-lg-block col-2'>
+                    <div className="d-none d-lg-block col-2">
                       <p>
                         <DateFormatter date={complaint.last_modified} />
                       </p>
                     </div>
 
-                    <div className='d-block d-lg-none col-12'>
+                    <div className="d-block d-lg-none col-12">
                       <p>ID: {complaint.id}</p>
                     </div>
-                    <div className='d-block d-lg-none col-12'>
+                    <div className="d-block d-lg-none col-12">
                       <p>Title: {complaint.title}</p>
                     </div>
-                    <div className='d-block d-lg-none col-12'>
+                    <div className="d-block d-lg-none col-12">
                       <p>Category Name: {complaint.categoryName}</p>
                     </div>
-                    <div className='d-block d-lg-none col-12'>
+                    <div className="d-block d-lg-none col-12">
                       <p>User: {complaint.userName}</p>
                     </div>
-                    <div className='d-block d-lg-none col-12'>
-                      <p>Date Created: <DateFormatter date={complaint.date_created} /></p>
+                    <div className="d-block d-lg-none col-12">
+                      <p>
+                        Date Created:{" "}
+                        <DateFormatter date={complaint.date_created} />
+                      </p>
                     </div>
-                    <div className='d-block d-lg-none col-12'>
-                      <p>Last Updated: <DateFormatter date={complaint.last_modified} /></p>
+                    <div className="d-block d-lg-none col-12">
+                      <p>
+                        Last Updated:{" "}
+                        <DateFormatter date={complaint.last_modified} />
+                      </p>
                     </div>
                   </div>
                 </Link>
               </div>
-            )
-          }
-
+            ))}
         </div>
       </div>
-      <div className='row'>
-        <div className='col-12'>
-        <PaginationHandler currentPage={currentPage} elementsPerPage={elementsPerPage} dataLength={filteredArray.length} />
+      <div className="row">
+        <div className="col-12">
+          <PaginationHandler
+            currentPage={currentPage}
+            elementsPerPage={elementsPerPage}
+            dataLength={filteredArray.length}
+          />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Complaints
+export default Complaints;
