8c48059947bb7aabbeefe78f16502f579f81a47f
YuriSereda777 <lomaj3ch@hotmail.com>
2023-06-09 09:41:12 +0300

    Update Complaints.js

--- src/pages/admin/Complaints.js
+++ src/pages/admin/Complaints.js
@@ -45,8 +45,8 @@ const Complaints = () => {
   ];
 
   const badges = [
-    { label: 'Pending', attr: 'status', value: '0' },
-    { label: 'Closed', attr: 'status', value: '1' }
+    { label: 'Pending', attr: 'status', value: 0 },
+    { label: 'Closed', attr: 'status', value: 1 }
   ];
   
   return (
