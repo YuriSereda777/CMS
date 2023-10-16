e5ea6a4ba1e6542f42e4aba8a72b235f7738dbf1
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-17 02:20:58 +0300

    Create Category (just admin) & Get All Categories

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,33 @@
+const Category = require("../models/Category");
+const { checkPermissions } = require("../utils");
+
+createCategory = async (req, res) => {
+  try {
+    checkPermissions(req.user, req.user.userId);
+
+    const { name } = req.body;
+
+    const category = new Category({
+      name,
+    });
+
+    const savedCategory = await category.save();
+    res.json(savedCategory);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to fetch categories" });
+  }
+};
+
+module.exports = {
+  createCategory,
+  getAllCategories,
+};
