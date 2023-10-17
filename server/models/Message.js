ee3511c2b52892f2a752e72b5a9adf7e8e106974
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-17 06:33:37 +0300

    add Message Schema

---
+++ server/models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  complaintId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Complaint",
+    required: true,
+  },
+  from: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Message", messageSchema);
