8bb3cf2e2a0c953675efdae54df4916a459c7717
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 02:19:33 +0300

    fix route

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/complaints", complaintRouter);
 app.use("/api/v1/messages", messageRouter);
-app.use("/api/v1/", userRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
