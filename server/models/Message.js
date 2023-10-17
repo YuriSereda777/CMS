cb7559bc9a9d76153d958079f7d965b9a4ffb6f6
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-17 08:41:38 +0300

    make it String instead of user id

--- server/models/Message.js
+++ server/models/Message.js
@@ -7,8 +7,7 @@ const messageSchema = new mongoose.Schema({
     required: true,
   },
   from: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    type: String,
     required: true,
   },
   text: {
