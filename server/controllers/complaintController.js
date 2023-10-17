58302e3dd9a9d9e44c0e8909ec6bfa8e248d90f8
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-17 04:07:34 +0300

    fix everything about complaint controller

--- server/controllers/complaintController.js
+++ server/controllers/complaintController.js
@@ -2,45 +2,75 @@ const Complaint = require("../models/Complaint");
 
 const createComplaint = async (req, res) => {
   try {
-    const { title, categoryId } = req.body;
+    const { title, category } = req.body;
+    const user = req.user._id;
+
     const complaint = new Complaint({
       title,
-      categoryId,
-      userId: req.user._id,
+      category,
+      user,
     });
 
     const savedComplaint = await complaint.save();
-    res.status(201).json(savedComplaint);
+    res.json(savedComplaint);
   } catch (error) {
-    res.status(400).json({ error: "Unable to create a complaint" });
+    res.status(400).json({ message: "Unable to create a complaint" });
   }
 };
 
 const getAllComplaints = async (req, res) => {
   try {
     const userId = req.user._id;
-    const complaints = await Complaint.find({ userId });
+    const userRole = req.user.role;
+
+    let complaints;
+
+    if (userRole === "admin") {
+      complaints = await Complaint.find()
+        .populate("user", "-password")
+        .populate("category", "name");
+    } else {
+      complaints = await Complaint.find({ user: userId }).populate(
+        "category",
+        "name"
+      );
+    }
+
     res.status(200).json(complaints);
   } catch (error) {
-    res.status(500).json({ error: "Unable to fetch complaints" });
+    res.status(500).json({ message: "Unable to fetch complaints" });
   }
 };
 
 const getComplaintById = async (req, res) => {
   try {
     const complaintId = req.params.id;
+    const userId = req.user._id;
+    const userRole = req.user.role;
 
-    const complaint = await Complaint.findById(complaintId)
-      .populate("userId", "-password")
-      .populate("categoryId", "name");
+    const complaint = await Complaint.findById(complaintId).populate(
+      "category",
+      "name"
+    );
 
     if (!complaint) {
-      return res.status(404).json({ error: "Complaint not found" });
+      return res.status(404).json({ message: "Complaint not found" });
+    }
+
+    if (
+      complaint.user.toString() !== userId.toString() &&
+      userRole !== "admin"
+    ) {
+      return res
+        .status(401)
+        .json({
+          message: "You do not have permission to access this complaint",
+        });
     }
 
     res.status(200).json(complaint);
   } catch (error) {
-    res.status(500).json({ error: "Unable to fetch the complaint" });
+    res.status(500).json({ message: "Unable to fetch the complaint" });
   }
 };
 
