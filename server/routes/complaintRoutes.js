c2b1bdb5ed7608c092c45e16dbb559ab44d8f4f9
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 02:16:27 +0300

    add complaint per month

--- server/routes/complaintRoutes.js
+++ server/routes/complaintRoutes.js
@@ -3,12 +3,15 @@ const {
   createComplaint,
   getAllComplaints,
   getComplaintById,
+  getComplaintsPerMonth,
 } = require("../controllers/complaintController");
 const authenticateUser = require("../middleware/authenticateUser");
 
 router.post("/", authenticateUser, createComplaint);
 
 router.get("/", authenticateUser, getAllComplaints);
+
+router.get("/complaints-per-month", authenticateUser, getComplaintsPerMonth);
 
 router.get("/:id", authenticateUser, getComplaintById);
 
