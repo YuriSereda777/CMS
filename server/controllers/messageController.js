019cc04ac9eed594b3147c501677507b6edd7013
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-17 06:56:30 +0300

    createMessage & getMessagesByComplaintId

---
+++ server/controllers/messageController.js
@@ -0,0 +1,36 @@
+const Message = require("../models/Message");
+
+const createMessage = async (req, res) => {
+  try {
+    const { complaintId, text } = req.body;
+    const from = req.user._id;
+
+    const message = new Message({
+      complaintId,
+      from,
+      text,
+    });
+
+    const savedMessage = await message.save();
+    res.status(201).json(savedMessage);
+  } catch (error) {
+    res.status(400).json({ error: "Unable to create a message" });
+  }
+};
+
+const getMessagesByComplaintId = async (req, res) => {
+  try {
+    const complaintId = req.params.complaintId;
+
+    const messages = await Message.find({ complaintId });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: "Unable to fetch messages" });
+  }
+};
+
+module.exports = {
+  createMessage,
+  getMessagesByComplaintId,
+};
