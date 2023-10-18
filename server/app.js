d8b2c8b55061a06fbd8bd5b1ee30a103f986ea63
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 03:38:40 +0300

    getTotalNumbers added

--- server/app.js
+++ server/app.js
@@ -18,6 +18,7 @@ const categoryRouter = require("./routes/categoryRoutes");
 const complaintRouter = require("./routes/complaintRoutes");
 const messageRouter = require("./routes/messageRoutes");
 const userRouter = require("./routes/userRoutes");
+const { getTotalNumbers } = require("./controllers/totalNumbersController");
 
 const app = express();
 
@@ -68,6 +69,7 @@ app.use("/api/v1/categories", categoryRouter);
 app.use("/api/v1/complaints", complaintRouter);
 app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/users", userRouter);
+app.get("/api/v1/total-numbers", getTotalNumbers);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
