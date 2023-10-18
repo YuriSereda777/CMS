80ae90feca9c7d97f72b198c50ef1e81e8cfc24d
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 04:40:12 +0300

    used the new api in the users page + fixed an error

--- frontend/src/pages/admin/Users.jsx
+++ frontend/src/pages/admin/Users.jsx
@@ -1,12 +1,12 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useHTTP from "../../hooks/useHttp";
 import useSearch from "../../hooks/useSearch";
 import Table from "../../components/Table";
-import useAxios from "../../hooks/useAxios";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [sortBy, setSortBy] = useState("id");
+  const { isLoading, error, sendRequest: getUsers } = useHTTP();
   const { setOriginalArray, filterArray, filteredArray, inputValue } =
     useSearch("name");
 
@@ -17,39 +17,42 @@ const Users = () => {
     setSortBy(value);
   };
 
-  // useEffect(() => {
-  //   getUsers({ url: "http://localhost:80/cms-api/users.php" }, (data) => {
-  //     setUsers(
-  //       data.map((element) => ({ ...element, id: parseInt(element.id) }))
-  //     );
-  //     setOriginalArray(
-  //       data.map((element) => ({ ...element, id: parseInt(element.id) }))
-  //     );
-  //   });
-  // }, [getUsers]);
+  useEffect(() => {
+    getUsers(
+      { url: "http://localhost:5000/api/v1/users/all-users" },
+      (data) => {
+        console.log(data);
+        const newData = data.map((element) => ({
+          id: element._id,
+          name: element.firstName + " " + element.lastName,
+          email: element.email,
+          phone: element.phone,
+          createdAt: element.createdAt,
+        }));
 
-  const {
-    data: totalUsers,
-    loading: totalUsersIsLoading,
-    error: totalUsersHasError,
-  } = useAxios("http://localhost:5000/api/v1/users/all-users", "GET");
+        setUsers(newData);
+        setOriginalArray(newData);
+      }
+    );
+  }, [getUsers]);
 
   const table = [
-    { colSize: 1, label: "ID", value: "id" },
+    { colSize: 3, label: "ID", value: "id" },
     { colSize: 2, label: "Name", value: "name" },
     { colSize: 3, label: "Email", value: "email" },
     { colSize: 2, label: "Phone", value: "phone" },
     { colSize: 2, label: "Created At", value: "created_at", isDate: true },
   ];
+
   return (
     <Table
       title="Users"
       table={table}
-      elements={totalUsers?.users}
+      elements={users}
       elementsPerPage={10}
       pagination={true}
-      isLoading={totalUsersIsLoading}
-      error={totalUsersHasError}
+      isLoading={isLoading}
+      error={error}
       search={true}
       searchInputValue={inputValue}
       searchHandler={searchHandler}
