c861d222207f8d0e4cbddb26d5b636a5a75d8f62
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 06:08:14 +0300

    deleted footer icons

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,12 +1,4 @@
-import React from "react";
-
 const Footer = () => {
-  const icons = [
-    { icon: "fab fa-facebook-square fa-fw", url: "#" },
-    { icon: "fab fa-google-plus-g fa-fw", url: "#" },
-    { icon: "fab fa-twitter fa-fw", url: "#" },
-  ];
-
   return (
     <footer className="pb-4">
       <div className="container">
