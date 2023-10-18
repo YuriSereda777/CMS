18e19754fb8717940b7ee508eb811f6a959e3cdf
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 03:17:33 +0300

    delete useHTTP

--- frontend/src/pages/admin/Users.jsx
+++ frontend/src/pages/admin/Users.jsx
@@ -2,11 +2,11 @@ import React, { useCallback, useEffect, useState } from "react";
 import useHTTP from "../../hooks/useHttp";
 import useSearch from "../../hooks/useSearch";
 import Table from "../../components/Table";
+import useAxios from "../../hooks/useAxios";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [sortBy, setSortBy] = useState("id");
-  const { isLoading, error, sendRequest: getUsers } = useHTTP();
   const { setOriginalArray, filterArray, filteredArray, inputValue } =
     useSearch("name");
 
@@ -17,35 +17,39 @@ const Users = () => {
     setSortBy(value);
   };
 
-  useEffect(() => {
-    getUsers({ url: "http://localhost:80/cms-api/users.php" }, (data) => {
-      setUsers(
-        data.map((element) => ({ ...element, id: parseInt(element.id) }))
-      );
-      setOriginalArray(
-        data.map((element) => ({ ...element, id: parseInt(element.id) }))
-      );
-    });
-  }, [getUsers]);
+  // useEffect(() => {
+  //   getUsers({ url: "http://localhost:80/cms-api/users.php" }, (data) => {
+  //     setUsers(
+  //       data.map((element) => ({ ...element, id: parseInt(element.id) }))
+  //     );
+  //     setOriginalArray(
+  //       data.map((element) => ({ ...element, id: parseInt(element.id) }))
+  //     );
+  //   });
+  // }, [getUsers]);
+
+  const {
+    data: totalUsers,
+    loading: totalUsersIsLoading,
+    error: totalUsersHasError,
+  } = useAxios("http://localhost:5000/api/v1/users/all-users", "GET");
 
   const table = [
     { colSize: 1, label: "ID", value: "id" },
-    { colSize: 2, label: "National ID", value: "nationalId" },
     { colSize: 2, label: "Name", value: "name" },
     { colSize: 3, label: "Email", value: "email" },
     { colSize: 2, label: "Phone", value: "phone" },
     { colSize: 2, label: "Created At", value: "created_at", isDate: true },
   ];
-
   return (
     <Table
       title="Users"
       table={table}
-      elements={users}
+      elements={totalUsers?.users}
       elementsPerPage={10}
       pagination={true}
-      isLoading={isLoading}
-      error={error}
+      isLoading={totalUsersIsLoading}
+      error={totalUsersHasError}
       search={true}
       searchInputValue={inputValue}
       searchHandler={searchHandler}
