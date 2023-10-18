b1d44930ace115f7cdaae1d1d4a40ac386d7aabc
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-18 05:05:34 +0300

    used the new api in the complaints page

--- frontend/src/pages/admin/Complaints.jsx
+++ frontend/src/pages/admin/Complaints.jsx
@@ -1,12 +1,17 @@
-import React, { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import useSearch from "../../hooks/useSearch";
-import useHttp from "../../hooks/useHttp";
 import Table from "../../components/Table";
+import useAxios from "../../hooks/useAxios";
 
 const Complaints = () => {
   const [complaints, setComplaints] = useState([]);
   const [sortBy, setSortBy] = useState("id");
-  const { isLoading, error, sendRequest: getComplaints } = useHttp();
+  const {
+    data,
+    loading: isLoading,
+    error,
+  } = useAxios(`http://localhost:5000/api/v1/complaints`, "GET");
+
   const { setOriginalArray, filterArray, filteredArray, inputValue } =
     useSearch("title");
   const [resultAfterFiler, setResultAfterFiler] = useState(filteredArray);
@@ -27,30 +32,35 @@ const Complaints = () => {
   };
 
   useEffect(() => {
-    getComplaints(
-      { url: "http://localhost:80/cms-api/complaints.php" },
-      (data) => {
-        setComplaints(
-          data.map((element) => ({ ...element, id: parseInt(element.id) }))
-        );
-        setOriginalArray(
-          data.map((element) => ({ ...element, id: parseInt(element.id) }))
-        );
-      }
-    );
-  }, [getComplaints]);
+    if (data) {
+      const newData = data.map((element) => {
+        return {
+          id: element._id,
+          title: element.title,
+          category: element.category,
+          user: element.user,
+          date_created: element.date_created,
+          status: element.status,
+        };
+      });
+      console.log(data);
+      console.log(newData);
+      setComplaints(newData);
+      setOriginalArray(newData);
+    }
+  }, [data]);
 
   useEffect(() => {
     setResultAfterFiler(filteredArray);
   }, [filteredArray]);
 
   const table = [
-    { colSize: 1, label: "ID", value: "id" },
+    { colSize: 2, label: "ID", value: "id" },
     { colSize: 3, label: "Title", value: "title" },
     { colSize: 2, label: "Category", value: "categoryName" },
     { colSize: 2, label: "User", value: "userName" },
     { colSize: 2, label: "Created At", value: "date_created", isDate: true },
-    { colSize: 2, label: "Status", value: "status", isStatus: true },
+    { colSize: 1, label: "Status", value: "status", isStatus: true },
   ];
 
   const badges = [
