6fc288bb7e4d6fd9b1c7acf24c9ef147de241d0a
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-19 04:53:08 +0300

    fixed the dashboard page

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,109 +1,73 @@
-import { useCallback, useEffect, useState } from "react";
-
-import "./Dashboard.css";
-import { Chart } from "../../UI/Chart";
-import useHTTP from "../../hooks/useHttp";
-import Loading from "../../UI/Loading";
+import { useEffect, useState, useMemo } from "react";
 import useAxios from "../../hooks/useAxios";
+import { Chart } from "../../UI/Chart";
+import Loading from "../../UI/Loading";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-  // const cards = [
-  //   { icon: "fa-solid fa-users" },
-  //   { icon: "fa-solid fa-user-shield" },
-  //   { icon: "fa-solid fa-envelope" },
-  //   { icon: "fa-solid fa-list" },
-  // ];
+  const cards = useMemo(
+    () => [
+      { icon: "fa-solid fa-user-shield" },
+      { icon: "fa-solid fa-users" },
+      { icon: "fa-solid fa-envelope" },
+      { icon: "fa-solid fa-list" },
+    ],
+    []
+  );
 
-  console.log("first");
-
-  // const [totalComplaints, setTotalComplaints] = useState([]);
-  // const {
-  //   isLoading: totalComplaintsIsLoading,
-  //   error: totalComplaintsHasError,
-  //   sendRequest: getTotalComplaints,
-  // } = useHTTP();
-
-  // const [totalUsers, setTotalUsers] = useState([]);
-  // const {
-  //   isLoading: totalUsersIsLoading,
-  //   error: totalUsersHasError,
-  //   sendRequest: getTotalUsers,
-  // } = useHTTP();
-
-  // const [totalNumbers, setTotalNumbers] = useState([]);
-  // const {
-  //   isLoading: totalNumbersIsLoading,
-  //   error: totalNumbersHasError,
-  //   sendRequest: getTotalNumbers,
-  // } = useHTTP();
-
-  // const totalComplaintsHandler = useCallback((data) => {
-  //   setTotalComplaints(data);
-  // }, []);
-
-  // const totalUsersHandler = useCallback((data) => {
-  //   setTotalUsers(data);
-  // }, []);
-
-  // let result = [];
-  // let i = 0;
-
-  // const totalNumbersHandler = useCallback((data) => {
-  //   setTotalNumbers(data);
-
-  //   for (const [, value] of Object.entries(data)) {
-  //     for (var v in value) {
-  //       result.push({
-  //         label: Object.keys(value[v])[0],
-  //         total: parseInt(Object.values(value[v])[0]),
-  //         cardIcon: cards[i].icon,
-  //       });
-  //     }
-
-  //     setTotalNumbers(result);
-  //     i++;
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   getTotalComplaints(
-  //     { url: "http://localhost:5000/api/v1/complaints/complaints-per-month" },
-  //     totalComplaintsHandler
-  //   );
-  //   getTotalUsers(
-  //     { url: "http://localhost:5000/api/v1/users/users-per-month" },
-  //     totalUsersHandler
-  //   );
-  // }, [
-  //   getTotalComplaints,
-  //   totalComplaintsHandler,
-  //   getTotalUsers,
-  //   totalUsersHandler,
-  //   getTotalNumbers,
-  //   totalNumbersHandler,
-  // ]);
+  const [totals, setTotals] = useState();
+  const [totalsLoading, setTotalsLoading] = useState(true);
 
   const {
-    data: totalComplaints,
-    loading: totalComplaintsIsLoading,
-    error: totalComplaintsHasError,
-  } = useAxios("http://localhost:5000/api/v1/complaints", "GET");
-  console.log(totalComplaints);
+    data: totalNumbers,
+    error: totalNumbersHasError,
+  } = useAxios(`http://localhost:5000/api/v1/total-numbers`, "GET");
+
+  useEffect(() => {
+    if (totalNumbers) {
+      const mergedArray = cards.map((card, index) => {
+        const total = Object.values(totalNumbers)[index];
+        return { ...card, total };
+      });
+      console.log(mergedArray);
+      setTotals(mergedArray);
+      setTotalsLoading(false);
+    }
+  }, [cards, totalNumbers]);
 
   const {
-    data: totalUsers,
-    loading: totalUsersIsLoading,
-    error: totalUsersHasError,
-  } = useAxios("http://localhost:5000/api/v1/users/all-users", "GET");
+    data: complaintsPerMonth,
+    loading: complaintsPerMonthLoading,
+    error: complaintsPerMonthHasError,
+  } = useAxios(
+    `http://localhost:5000/api/v1/complaints/complaints-per-month`,
+    "GET"
+  );
 
-  console.log(totalUsers);
+  const {
+    data: usersPerMonth,
+    loading: usersPerMonthLoading,
+    error: usersPerMonthHasError,
+  } = useAxios(`http://localhost:5000/api/v1/users/users-per-month`, "GET");
+
+  if (totalsLoading || complaintsPerMonthLoading || usersPerMonthLoading) {
+    return <Loading />;
+  }
+
+  if (
+    totalNumbersHasError ||
+    complaintsPerMonthHasError ||
+    usersPerMonthHasError
+  ) {
+    return <div>An error occurred.</div>;
+  }
 
   return (
     <div className="admins">
       <h1 className="mb-5">Dashboard</h1>
       <div className="cards">
-        {/* <div className="row">
-          {totalNumbers.map((item, index) => (
+        <div className="row">
+          {totals.map((item, index) => (
             <div className="col-12 col-sm-6 col-lg-3 mb-4" key={index}>
               <div className="card">
                 <i className={item.cardIcon}></i>
@@ -112,16 +76,15 @@ const Dashboard = () => {
               </div>
             </div>
           ))}
-        </div> */}
-
-        {/* <div className="row mt-5">
+        </div>
+        <div className="row mt-5">
           <div className="col-12 col-xl-6">
-            <Chart label="Complaints" data={totalComplaints?.count} />
+            <Chart label="Complaints" data={complaintsPerMonth} />
           </div>
           <div className="col-12 col-xl-6">
-            <Chart label="Users" data={totalUsers.count} />
+            <Chart label="Users" data={usersPerMonth} />
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
