b47ec68c98336ea3f51c81c082227a544ae21b13
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 04:47:45 +0300

    TableBadges component

---
+++ frontend/src/components/Table/TableBadges.jsx
@@ -0,0 +1,26 @@
+import Badge from "../../UI/Badge";
+
+const TableBadges = ({ badges, activeFilter, filterHandler }) => {
+  return (
+    <ul className="flex flex-row gap-3">
+      {badges.map((badge, index) => (
+        <li key={index}>
+          {activeFilter === badge.value ? (
+            <Badge
+              className=" active"
+              text={badge.label}
+              onClick={() => filterHandler(badge.attr, badge.value)}
+            />
+          ) : (
+            <Badge
+              text={badge.label}
+              onClick={() => filterHandler(badge.attr, badge.value)}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TableBadges;
