55d56d218eda4daa901e6ba1c82dde6b3e89c2e7
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 15:04:36 +0300

    AdminNav tailwind classes

--- frontend/src/components/Navbar/AdminNav.jsx
+++ frontend/src/components/Navbar/AdminNav.jsx
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { selectUser, logout } from "../../store/slices/userAuthSlice";
 import { adminMenu } from "../../data/lists";
 import adminPic from "../../imgs/admin.png";
-import "./AdminNav.css";
 import { Spin as Hamburger } from "hamburger-react";
 import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
 const Sidebar = (props) => {
   const user = useSelector(selectUser);
   const adminName = `${user.firstName} ${user.lastName}`;
@@ -19,36 +19,74 @@ const Sidebar = (props) => {
   };
 
   return (
-    <div className={props.menuIsOpened ? "sidebar sidebar-opened" : "sidebar"}>
-      <div className="absolute top-10 right-5">
+    <div
+      className={`fixed h-screen pt-24 bg-gray-700 text-gray-300 overflow-hidden transition-all duration-[1.2s] sidebar-opened
+          ${props.menuIsOpened ? "w-[300px]" : "w-[120px]"}`}
+    >
+      <div
+        className={`absolute top-10 right-5 transition-all duration-[1.2s]  ${
+          props.menuIsOpened ? "right-5" : "right-1/2 translate-x-1/2"
+        }`}
+      >
         <Hamburger
           toggled={props.menuIsOpened}
           toggle={props.setMenuIsOpened}
           size={22}
         />
       </div>
-      <h1>Admin Panel</h1>
-      <div className="sidebar-header">
-        <div className="admin d-flex align-items-center">
-          <div className="admin-info d-flex justify-content-center m-auto">
-            <div className="admin-pic m-auto">
-              <img src={adminPic} alt="Admin" />
-            </div>
-            <div className={"d-flex align-items-center pe-0"}>
-              <div>
-                <p className="admin-name mb-2">{adminName}</p>
-                <p className="admin-role">Administrator</p>
-              </div>
+      <h1
+        className={`pl-16 text-base text-gray-200 uppercase whitespace-nowrap overflow-hidden transition-all duration-[1.2s] ${
+          props.menuIsOpened ? "w-full" : "w-0"
+        }`}
+      >
+        Admin Panel
+      </h1>
+      <div
+        className={`min-w-[40px] pt-[15px] pb-[25px] transition-all duration-[1.2s] ${
+          props.menuIsOpened ? "px-[35px]" : "px-[30px]"
+        }`}
+      >
+        <div className="py-[25px] flex flex-row items-center border-y border-gray-100">
+          <div
+            className={`mx-auto flex flex-row justify-center overflow-hidden transition-all duration-[1.2s] ${
+              props.menuIsOpened ? "gap-3" : "gap-0"
+            }`}
+          >
+            <img src={adminPic} alt="Admin" className="w-[50px] mx-auto" />
+            <div
+              className={`flex flex-col gap-1 transition-all duration-[1.2s] ${
+                props.menuIsOpened ? "w-full" : "w-0"
+              }`}
+            >
+              <p className="text-gray-100 text-base whitespace-nowrap overflow-hidden transition duration-[1.2s]">
+                {adminName}
+              </p>
+              <p className="text-gray-300 text-sm whitespace-nowrap overflow-hidden transition duration-[1.2s]">
+                Administrator
+              </p>
             </div>
           </div>
         </div>
       </div>
-      <ul>
+      <ul className="flex flex-col gap-3">
         {adminMenu.map((item, index) => (
           <li key={index}>
-            <NavLink to={item.path} className="link d-flex align-items-center">
-              {item.icon}
-              <span className="d-inline-block">{item.name}</span>
+            <NavLink
+              to={item.path}
+              className={`pt-[13px] flex flex-row items-center gap-2 whitespace-nowrap overflow-hidden transition-all duration-[1.2s] group ${
+                props.menuIsOpened
+                  ? "pb-[13px] px-[65px]"
+                  : "pr-0 pb-[13px] pl-[43px]"
+              }`}
+            >
+              <item.icon className="group-hover:animate-swing" />
+              <span
+                className={`text-gray-300 text-base tracking-wider whitespace-nowrap overflow-hidden transition-all duration-700 ${
+                  props.menuIsOpened ? "w-full" : "w-0"
+                }`}
+              >
+                {item.name}
+              </span>
             </NavLink>
           </li>
         ))}
@@ -58,9 +96,21 @@ const Sidebar = (props) => {
           style={{ cursor: "pointer" }}
           onClick={logoutHandler}
         >
-          <a className="link d-flex align-items-center">
-           <FaArrowUpRightFromSquare />
-            <span>Logout</span>
+          <a
+            className={`pt-[13px] flex flex-row items-center gap-2 whitespace-nowrap overflow-hidden transition-all duration-[1.2s] group ${
+              props.menuIsOpened
+                ? "pb-[13px] px-[65px]"
+                : "pr-0 pb-[13px] pl-[43px]"
+            }`}
+          >
+            <FaArrowUpRightFromSquare />
+            <span
+              className={`text-gray-300 text-base tracking-wider whitespace-nowrap overflow-hidden transition-all duration-700 ${
+                props.menuIsOpened ? "w-full" : "w-0"
+              }`}
+            >
+              Logout
+            </span>
           </a>
         </li>
       </ul>
