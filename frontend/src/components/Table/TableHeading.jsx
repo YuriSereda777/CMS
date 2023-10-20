421930a24c287921e23692cb4a1bde667d1dd41b
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 06:12:15 +0300

    updated TableHeading.jsx

--- frontend/src/components/Table/TableHeading.jsx
+++ frontend/src/components/Table/TableHeading.jsx
@@ -2,9 +2,9 @@ import { FaChevronDown } from "react-icons/fa";
 
 const TableHeading = ({ table, sortHandler, sortBy }) => {
   return (
-    <div className="mb-3 pb-1.5 hidden lg:flex lg:flex-row border-b-[0.5px] border-gray-400">
+    <div className="mb-3 pb-1.5 hidden lg:grid lg:grid-cols-12 border-b-[0.5px] border-gray-400">
       {table.map((heading, index) => (
-        <div className={`col-${heading.colSize}`} key={index}>
+        <div className={heading.colSize} key={index}>
           <p
             className="flex flex-row  items-center gap-3 text-xl font-semibold cursor-pointer"
             onClick={() => sortHandler(heading.value)}
