64d9716abc53db1c9e235a93ea9389a5d72f56cc
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 04:05:23 +0300

    TableHeading tailwind classes

---
+++ frontend/src/components/Table/TableHeading.jsx
@@ -0,0 +1,21 @@
+import { FaChevronDown } from "react-icons/fa";
+
+const TableHeading = ({ table, sortHandler, sortBy }) => {
+  return (
+    <div className="mb-3 hidden lg:flex lg:flex-row border-b-[0.5px] border-gray-400">
+      {table.map((heading, index) => (
+        <div className={`col-${heading.colSize}`} key={index}>
+          <p
+            className="flex flex-row  items-center gap-3 text-xl font-semibold cursor-pointer"
+            onClick={() => sortHandler(heading.value)}
+          >
+            {heading.label}
+            {heading.value === sortBy && <FaChevronDown className="text-lg" />}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TableHeading;
