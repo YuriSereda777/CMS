9afc788b2f1af70dbfe8e757606ce896f4f12280
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 06:11:43 +0300

    updated pagesGrid.js

--- frontend/src/data/pagesGrid.js
+++ frontend/src/data/pagesGrid.js
@@ -1,30 +1,45 @@
 export const adminsPageGrid = [
-  { colSize: 2, label: "ID", value: "id" },
-  { colSize: 3, label: "Name", value: "name" },
-  { colSize: 3, label: "Email", value: "email" },
-  { colSize: 2, label: "Phone", value: "phone" },
-  { colSize: 2, label: "Created At", value: "created_at", isDate: true },
+  { colSize: "col-span-2", label: "ID", value: "id" },
+  { colSize: "col-span-3", label: "Name", value: "name" },
+  { colSize: "col-span-3", label: "Email", value: "email" },
+  { colSize: "col-span-2", label: "Phone", value: "phone" },
+  {
+    colSize: "col-span-2",
+    label: "Created At",
+    value: "created_at",
+    isDate: true,
+  },
 ];
 
 export const usersPagesGrid = [
-  { colSize: 3, label: "ID", value: "id" },
-  { colSize: 2, label: "Name", value: "name" },
-  { colSize: 3, label: "Email", value: "email" },
-  { colSize: 2, label: "Phone", value: "phone" },
-  { colSize: 2, label: "Created At", value: "created_at", isDate: true },
+  { colSize: "col-span-3", label: "ID", value: "id" },
+  { colSize: "col-span-2", label: "Name", value: "name" },
+  { colSize: "col-span-3", label: "Email", value: "email" },
+  { colSize: "col-span-2", label: "Phone", value: "phone" },
+  {
+    colSize: "col-span-2",
+    label: "Created At",
+    value: "created_at",
+    isDate: true,
+  },
 ];
 
 export const categoriesPageGrid = [
-  { colSize: 3, label: "ID", value: "id" },
-  { colSize: 5, label: "Title", value: "name" },
-  { colSize: 4, label: "Complaints", value: "number" },
+  { colSize: "col-span-3", label: "ID", value: "id" },
+  { colSize: "col-span-5", label: "Title", value: "name" },
+  { colSize: "col-span-4", label: "Complaints", value: "number" },
 ];
 
 export const complaintsPageGrid = [
-  { colSize: 2, label: "ID", value: "id" },
-  { colSize: 3, label: "Title", value: "title" },
-  { colSize: 2, label: "Category", value: "categoryName" },
-  { colSize: 2, label: "User", value: "userName" },
-  { colSize: 2, label: "Created At", value: "date_created", isDate: true },
-  { colSize: 1, label: "Status", value: "status", isStatus: true },
+  { colSize: "col-span-2", label: "ID", value: "id" },
+  { colSize: "col-span-3", label: "Title", value: "title" },
+  { colSize: "col-span-2", label: "Category", value: "categoryName" },
+  { colSize: "col-span-2", label: "User", value: "userName" },
+  {
+    colSize: "col-span-2",
+    label: "Created At",
+    value: "date_created",
+    isDate: true,
+  },
+  { colSize: "col-span-1", label: "Status", value: "status", isStatus: true },
 ];
