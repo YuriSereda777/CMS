ef8e87d680b15cb843d2a465fd1173cd801b4588
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-20 09:57:31 +0300

    updated TableSearchInput input width

--- frontend/src/components/Table/TableSearchInput.jsx
+++ frontend/src/components/Table/TableSearchInput.jsx
@@ -2,7 +2,7 @@ import SearchBar from "../../UI/SearchBar";
 
 const TableSearchInput = ({ searchInputValue, searchHandler }) => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3">
+    <div className="w-full md:w-1/2 xl:w-1/3">
       <SearchBar
         value={searchInputValue}
         onChange={(e) => searchHandler(e.target.value)}
