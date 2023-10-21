f1bf1b6b5584b3aa47f4abe4ca101e5dd15c6f33
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-21 13:19:27 +0300

    removed table heading dark mode classes

--- frontend/src/components/Table/TableHeading.jsx
+++ frontend/src/components/Table/TableHeading.jsx
@@ -3,17 +3,15 @@ import { FaChevronDown } from "react-icons/fa";
 const TableHeading = ({ table, sortHandler, sortBy }) => {
   return (
     <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400">
           <tr>
             {table.map((heading, index) => (
               <th
                 key={index}
                 scope="col"
                 className={`px-6 py-3 cursor-pointer ${
-                  heading.value === sortBy
-                    ? "text-gray-900 dark:text-white"
-                    : ""
+                  heading.value === sortBy && "text-gray-900"
                 }`}
                 onClick={() => sortHandler(heading.value)}
               >
