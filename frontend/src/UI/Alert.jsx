9bda7e63d166b6eb4651d5a7c24de6c0e1abc59e
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-22 06:47:17 +0300

    Alert.jsx tailwind classes

--- frontend/src/UI/Alert.jsx
+++ frontend/src/UI/Alert.jsx
@@ -1,22 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { FaCircleInfo } from "react-icons/fa6";
 
 const Alert = (props) => {
   return (
-    <Link
-      to={props.path}
-      className={`block mb-4 p-0 ${props.className ? props.className : ""}`}
-    >
+    <Link to={props.path} className={props.className || ""}>
       <div className="bg-primary-color text-white p-3 rounded-lg font-bold text-lg">
-        <div className="flex items-center">
-          {props.icon ? (
-            <div className="pr-2">
-              <i className="fa-solid fa-circle-info"></i>
-            </div>
-          ) : null}
-          <div className="flex-1">
-            <p className="inline-block">{props.children}</p>
-          </div>
+        <div className="flex items-center gap-2">
+          {props.icon && <FaCircleInfo />}
+          <p>{props.text}</p>
         </div>
       </div>
     </Link>
