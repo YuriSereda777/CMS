31b00ae2473602668608bd41dc3f3ab5444deb76
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-22 07:51:17 +0300

    increased Alert.jsx icon size

--- frontend/src/UI/Alert.jsx
+++ frontend/src/UI/Alert.jsx
@@ -4,10 +4,10 @@ import { FaCircleInfo } from "react-icons/fa6";
 const Alert = (props) => {
   return (
     <Link to={props.path} className={props.className || ""}>
-      <div className="bg-primary-color text-white p-3 rounded-lg font-bold text-lg">
+      <div className="p-3 bg-primary-color rounded-lg text-white">
         <div className="flex items-center gap-2">
-          {props.icon && <FaCircleInfo />}
-          <p>{props.text}</p>
+          {props.icon && <FaCircleInfo className="text-2xl" />}
+          <p className="text-lg font-semibold">{props.text}</p>
         </div>
       </div>
     </Link>
