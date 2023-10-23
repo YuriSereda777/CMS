fc84ad13338edce763415e68f15bed9d3ca8f2cb
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-23 12:49:54 +0300

    updated faq pages

--- frontend/src/components/FAQ/FAQCard.jsx
+++ frontend/src/components/FAQ/FAQCard.jsx
@@ -4,14 +4,10 @@ const FAQCard = ({ card }) => {
   return (
     <Link
       to={card.path}
-      className="faq-card relative py-8 flex flex-col items-center gap-3 border-2 rounded-2xl cursor-pointer transition-all duration-500 ease-out hover:before:opacity-100 before:absolute before:content-[''] before:inset-0 before:bg-gradient-to-r before:from-blue-800 before:to-sky-400 before:rounded-xl before:opacity-0 before:transition-all before:duration-500 before:ease-linear"
-      style={{
-        background:
-          "linear-gradient(white, white), linear-gradient(to right, #1e40af, #38bdf8)",
-      }}
+      className="faq-card relative py-8 flex flex-col items-center gap-3 border-2 rounded-2xl cursor-pointer transition-all duration-300 ease-out hover:bg-sky-500 group"
     >
-      <card.icon className="gradient-text-color relative z-[10] text-3xl transition duration-500" />
-      <p className="gradient-text-color relative z-[10] text-2xl font-semibold transition duration-500">
+      <card.icon className="group-hover:text-white relative z-[10] text-3xl text-gray-600 transition duration-300" />
+      <p className="group-hover:text-white relative z-[10] text-2xl text-gray-600 font-semibold transition duration-300">
         {card.text}
       </p>
     </Link>
