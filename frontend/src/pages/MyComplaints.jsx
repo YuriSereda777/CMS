0568b904413b579ed08f2f10b02ed98d6cb02a20
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-23 06:06:36 +0300

    my complaints page tailwind classes

--- frontend/src/pages/MyComplaints.jsx
+++ frontend/src/pages/MyComplaints.jsx
@@ -1,6 +1,4 @@
 import { Link, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectUser } from "../store/slices/userAuthSlice";
 import useAxios from "../hooks/useAxios";
 import Alert from "../UI/Alert";
 import Hero from "../UI/Hero";
@@ -9,15 +7,12 @@ import StatusFormatter from "../UI/StatusFormatter";
 import DateFormatter from "../UI/DateFormatter";
 import Loading from "../UI/Loading";
 import Error from "../UI/Error";
-import classes from "./MyComplaints.module.css";
 
 const MyComplaints = () => {
   let { page: currentPage } = useParams();
   const elementsPerPage = 10;
   const start = (currentPage - 1) * elementsPerPage;
   const end = start + elementsPerPage;
-
-  const user = useSelector(selectUser);
 
   const {
     data: userComplaints,
@@ -50,45 +45,40 @@ const MyComplaints = () => {
   return (
     <>
       <Hero title="My Complaints" />
-      <section className="complaints px-8 px-sm-0">
-        <div className="container">
-          <div className="row">
-            {user.role === "user" && (
-              <Alert path="/create-complaint" icon>
-                Need help? create a new complaint from here.
-              </Alert>
-            )}
-          </div>
-          <div className="flex flex-wrap gap-4">
-            {userComplaints.slice(start, end).map((complaint) => (
-              <div
-                key={complaint._id}
-                className={`w-full ${classes.complaint} mb-3 text-muted p-4`}
+      <section className="flex flex-col gap-6">
+        <Alert
+          path="/create-complaint"
+          icon={true}
+          text="Need help? create a new complaint from here."
+        />
+        <div className="flex flex-col gap-4">
+          {userComplaints.slice(start, end).map((complaint) => (
+            <div
+              key={complaint._id}
+              className="w-full pb-2 border-b last-of-type:border-b-0 text-gray-500 font-semibold transition duration-500 hover:text-gray-600"
+            >
+              <Link
+                to={`/complaint/${complaint._id}`}
+                className="md:grid md:grid-cols-12 items-center"
               >
-                <Link to={`/complaint/${complaint._id}`}>
-                  <div className="flex md:flex-row flex-col justify-between">
-                    <p className="text-lg font-semibold mb-2">
-                      {complaint.title}
-                    </p>
-                    <p className="complaint-category">{complaint.category}</p>
-                    <p className="complaint-date-created">
-                      <DateFormatter date={complaint.date_created} />
-                    </p>
-                    <p className="complaint-status">
-                      <StatusFormatter status={complaint.status} />
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-
-          <PaginationHandler
-            currentPage={currentPage}
-            elementsPerPage={elementsPerPage}
-            dataLength={userComplaints.length}
-          />
+                <p className="col-span-5 text-lg">{complaint.title}</p>
+                <p className="col-span-3">{complaint.category}</p>
+                <p className="col-span-3">
+                  <DateFormatter date={complaint.date_created} />
+                </p>
+                <p className="col-span-1 justify-self-end">
+                  <StatusFormatter status={complaint.status} />
+                </p>
+              </Link>
+            </div>
+          ))}
         </div>
+
+        <PaginationHandler
+          currentPage={currentPage}
+          elementsPerPage={elementsPerPage}
+          dataLength={userComplaints.length}
+        />
       </section>
     </>
   );
