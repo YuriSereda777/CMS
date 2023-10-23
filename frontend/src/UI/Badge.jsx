4c891c21a2227c0693db58bb38a21e5da6c85c72
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-23 06:54:32 +0300

    Badge.jsx tailwind classes

--- frontend/src/UI/Badge.jsx
+++ frontend/src/UI/Badge.jsx
@@ -1,7 +1,9 @@
 const Badge = ({ active, text, onClick }) => {
   return (
     <span
-      className={`badge ${active && "active"}`}
+      className={`py-2 px-5 bg-sky-600 rounded-2xl text-white cursor-pointer transition duration-300 hover:bg-sky-800 ${
+        active && "bg-sky-800"
+      }`}
       onClick={onClick ? onClick : () => {}}
     >
       {text}
