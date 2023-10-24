a29c94db02440005a27f3e9b8705884532d59ffa
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-24 06:42:57 +0300

    updated Badge.jsx

--- frontend/src/UI/Badge.jsx
+++ frontend/src/UI/Badge.jsx
@@ -4,7 +4,7 @@ const Badge = ({ active, text, onClick }) => {
       className={`py-2 px-5 bg-sky-600 rounded-2xl text-white cursor-pointer transition duration-300 hover:bg-sky-800 ${
         active && "bg-sky-800"
       }`}
-      onClick={onClick ? onClick : () => {}}
+      onClick={onClick || (() => {})}
     >
       {text}
     </span>
