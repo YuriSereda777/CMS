697bbbf5839b1998d3ef10287497ba9604765641
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-24 07:11:43 +0300

    added className prop to the layout section comopnent

--- frontend/src/layout/Section.jsx
+++ frontend/src/layout/Section.jsx
@@ -1,6 +1,10 @@
-const Section = ({ children }) => {
+const Section = ({ children, className }) => {
   return (
-    <section className="px-10 sm:px-16 md:px-28 lg:px-40 xl:px-60 py-12">
+    <section
+      className={`px-10 sm:px-16 md:px-28 lg:px-40 xl:px-60 py-12 ${
+        className || ""
+      }`}
+    >
       {children}
     </section>
   );
