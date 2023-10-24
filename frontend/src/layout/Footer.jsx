0786b028f588a8f18c641e7b6ea0ceecf33fc017
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-24 07:22:39 +0300

    footer layout component

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="mb-8 flex flex-row justify-center text-lg text-gray-600">
+      © 2023 CMS All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
