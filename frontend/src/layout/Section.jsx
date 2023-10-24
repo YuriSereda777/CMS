2e82dd2ca99ce29c51c5c72e7749a576b23f568b
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-24 07:10:58 +0300

    created section layout component

---
+++ frontend/src/layout/Section.jsx
@@ -0,0 +1,9 @@
+const Section = ({ children }) => {
+  return (
+    <section className="px-10 sm:px-16 md:px-28 lg:px-40 xl:px-60 py-12">
+      {children}
+    </section>
+  );
+};
+
+export default Section;
