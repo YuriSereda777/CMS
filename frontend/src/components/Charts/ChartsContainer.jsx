3ccb945e64a23c5dd966a23872b630f723138127
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-25 05:30:47 +0300

    ChartsContainer tailwind classes

--- frontend/src/components/Charts/ChartsContainer.jsx
+++ frontend/src/components/Charts/ChartsContainer.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import AreaChart from "./AreaChart";
 import BarChart from "./BarChart";
-import "./ChartsContainer.css";
 
 const ChartsContainer = ({ complaintsPerMonth, usersPerMonth }) => {
   const [barChart, setBarChart] = useState(true);
 
   return (
-    <div className="charts-container">
-      <h4 className="text-2xl">Monthly Complaints</h4>
-      <button onClick={() => setBarChart(!barChart)}>
+    <div className="mt-16 flex flex-col items-center">
+      <h4 className="text-2xl mb-2">Monthly Complaints</h4>
+      <button
+        className="text-lg text-sky-500"
+        onClick={() => setBarChart(!barChart)}
+      >
         {barChart ? "AreaChart" : "BarChart"}
       </button>
       {barChart ? (
