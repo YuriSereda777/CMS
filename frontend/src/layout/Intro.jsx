67a7587dab61f714a378f1eb5d1027c2655da580
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-25 10:07:06 +0300

    better colors and padding

--- frontend/src/layout/Intro.jsx
+++ frontend/src/layout/Intro.jsx
@@ -1,8 +1,8 @@
 const Intro = (props) => {
   return (
-    <div className=" max-w-xl flex flex-col gap-4">
-      <h1 className="text-5xl text-gray-200 font-bold">{props.title}</h1>
-      <p className="text-2xl text-gray-300">{props.text}</p>
+    <div className="max-w-xl pt-20 lg:pt-0 flex flex-col gap-4">
+      <h1 className="text-5xl text-gray-100 font-bold">{props.title}</h1>
+      <p className="text-2xl text-gray-50">{props.text}</p>
     </div>
   );
 };
