0151d34fd2e19101b0ae8c1eaf0dda6c902fc169
YuriSereda777 <lomaj3ch@hotmail.com>
2023-10-25 11:50:26 +0300

    fixed ui

--- frontend/src/UI/Hero.jsx
+++ frontend/src/UI/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = (props) => {
   return (
-    <div className="pb-12 md:py-36 flex flex-col gap-4 bg-sky-500 text-white text-center capitalize">
+    <div className="pb-12 pt-24 px-2  md:py-36 flex flex-col gap-4 bg-sky-500 text-white text-center capitalize">
       {props?.headTitle && <h1 className="text-4xl">{props.headTitle}</h1>}
       <h1 className="text-3xl">{props.title}</h1>
       {props.children}
